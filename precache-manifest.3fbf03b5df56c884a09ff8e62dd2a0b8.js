548ee3d0771d11588596e7280bcd1e9e27fd4fe8
Chris Hesterman <spoiler14@comcast.net>
2019-09-21 15:50:22 -0700

    Updates

---
+++ precache-manifest.3fbf03b5df56c884a09ff8e62dd2a0b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80affa06110487736eb5f23ed80358b7",
+    "url": "/PokeThing/index.html"
+  },
+  {
+    "revision": "f01b2b7d135da73ddf86",
+    "url": "/PokeThing/static/css/main.44330bb6.chunk.css"
+  },
+  {
+    "revision": "b8e253481088a8056963",
+    "url": "/PokeThing/static/js/2.623b8547.chunk.js"
+  },
+  {
+    "revision": "f01b2b7d135da73ddf86",
+    "url": "/PokeThing/static/js/main.53917a07.chunk.js"
+  },
+  {
+    "revision": "daf83a9b6c20e87f878e",
+    "url": "/PokeThing/static/js/runtime~main.a32dd4dd.js"
+  }
+]);
